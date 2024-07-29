2a7e639ec7775e1aaca0611e0d4e19ef0dcb8c5f
Pedro Ottoni <pdottoni@gmail.com>
2024-07-29 15:47:31 -0300

    Correção nomeCliente

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -19,13 +19,16 @@ public class ContaTerminal {
         System.out.println("Por favor, agora digite o número da sua conta: ");
         int numeroConta = sc.nextInt();
 
-        System.out.println("Por favor, informe o seu nome: ");
-        String nomeCliente = sc.next();
+        System.out.println("Por favor, informe o seu primeiro nome: ");
+        String primeiroNomeCliente = sc.next();
+
+        System.out.println("Por favor, informe o seu sobrenome: ");
+        String sobrenomeCliente = sc.next();
 
         System.out.println("Por fim, digite o seu saldo: ");
         double saldoConta = sc.nextDouble();
 
 
-        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
+        System.out.println("Olá, " + primeiroNomeCliente + " " + sobrenomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
     }
 }
